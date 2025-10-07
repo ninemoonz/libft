0b9b747d3138ac1a61b3f7c6e34048bb5d13296f
Koodal Kweon <kkweon@f0r4s22.codam.nl>
2025-10-07 17:17:35 +0200

    creating ft_strlcpy.c file

---
+++ ft_strlcpy.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 17:11:40 by kkweon            #+#    #+#             */
+/*   Updated: 2025/10/07 17:17:19 by kkweon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+size_t	ft_strlcpy(char *dest, char *src, unsigned int destsize)
+{
+}
+
+int	main(void)
+{
+	return (0);
+}
