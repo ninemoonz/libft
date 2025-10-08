fbb9f29fd50ef83232bd6fc0b6e7e0b65154fb47
Koodal Kweon <kkweon@f1r6s19.codam.nl>
2025-10-08 17:43:25 +0200

    creating ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 14:38:03 by kkweon            #+#    #+#             */
+/*   Updated: 2025/10/08 16:43:20 by kkweon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bsd/string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dst, char *src, unsigned int dstsize)
+{
+	unsigned int dst_len;
+	unsigned int src_len;
+	unsigned int tot_len;
+	unsigned int buffer_len;
+	unsigned int i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	tot_len = dst_len + src_len;
+	buffer_len = dstsize - dst_len - 1;
+	if (dst_len >= dstsize)
+		return (tot_len);
+	i = 0;
+	while (src[i] != '\0' && buffer_len--)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (tot_len);
+}
