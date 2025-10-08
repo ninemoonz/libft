45cb18206ecaac7f6577df2e7cf1a11e746ccc19
Koodal Kweon <kkweon@f1r6s19.codam.nl>
2025-10-08 18:33:40 +0200

    idk why it works

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 17:48:01 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/08 17:58:25 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/10/08 18:32:44 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,28 @@
 
 void	*ft_memset(void *dest, int c, size_t count)
 {
+	unsigned int	i;
+	unsigned char	*p;
+
+	p = dest;
+	i = 0;
+	while (count > 0)
+	{
+		*p = c;
+		p++;
+		count--;
+	}
+	return (dest);
 }
+
+// what is void pointer
 
 int	main(void)
 {
 	char buffer[] = "This is very important string";
+	printf("size of int is: %lu\n", sizeof(int));
 	printf("Before: %s\n", buffer);
-	memset(buffer, '-', 4);
+	ft_memset(buffer + 2, '8', 10 * sizeof(char));
 	printf("After: %s\n", buffer);
 	return (0);
 }
