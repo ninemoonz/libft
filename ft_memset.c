b618961fbb5241099c67f81b284de6442d4d01f4
Koodal Kweon <kkweon@f0r1s2.codam.nl>
2025-10-09 11:00:19 +0200

    creating ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 17:48:01 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/08 18:32:44 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/10/09 10:59:46 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-void	*ft_memset(void *dest, int c, size_t count)
+void	*ft_memset(void *dest, int c, unsigned int count)
 {
-	unsigned int	i;
-	unsigned char	*p;
+	unsigned int i;
+	char *char_p;
 
-	p = dest;
+	char_p = dest;
 	i = 0;
 	while (count > 0)
 	{
-		*p = c;
-		p++;
+		*char_p = c;
+		char_p++;
+		printf("%p\n", char_p);
 		count--;
 	}
 	return (dest);
-}
-
-// what is void pointer
-
-int	main(void)
-{
-	char buffer[] = "This is very important string";
-	printf("size of int is: %lu\n", sizeof(int));
-	printf("Before: %s\n", buffer);
-	ft_memset(buffer + 2, '8', 10 * sizeof(char));
-	printf("After: %s\n", buffer);
-	return (0);
 }
