e62170434beb99884ed0f79b64ffb9bf91912d1b
Koodal Kweon <kkweon@f0r1s2.codam.nl>
2025-10-09 11:40:37 +0200

    creating ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/08 17:48:01 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/09 11:01:12 by kkweon           ###   ########.fr       */
+/*   Created: 2025/10/09 11:01:06 by kkweon            #+#    #+#             */
+/*   Updated: 2025/10/09 11:40:14 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *dest, int c, unsigned int count)
+void	ft_bzero(void *str, unsigned int n)
 {
 	unsigned int i;
 	char *char_p;
 
-	char_p = dest;
+	char_p = str;
 	i = 0;
-	while (count > 0)
+	while (n > 0)
 	{
-		*char_p = c;
+		*char_p = 0;
 		char_p++;
 		printf("%p\n", char_p);
-		count--;
+		n--;
 	}
-	return (dest);
 }
