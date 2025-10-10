f224048e83f5e8084a3b9df03599257820811046
Koodal Kweon <kkweon@f1r5s4.codam.nl>
2025-10-10 17:11:17 +0200

    removing redundant file

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,23 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchar.c                                       :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 13:53:47 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/10 17:02:48 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/10/10 17:10:56 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-
 char	*ft_strchr(const char *str, int init)
 {
-	int		i;
-	char	*tmp_str;
+	int i;
+	char *tmp_str;
 
 	i = 0;
 	tmp_str = (char *)str;
@@ -27,16 +23,5 @@ char	*ft_strchr(const char *str, int init)
 			return (&tmp_str[i]);
 		i++;
 	}
-	return (tmp_str);
-}
-
-int	main(void)
-{
-	char str[] = "find something in this string please";
-	char *ptr;
-
-	ptr = ft_strchr(str, 'g');
-	printf("%s", ptr);
-
-	return (0);
+	return (NULL);
 }
