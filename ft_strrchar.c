3b87b7481c5014a71476c36f024bfc42d21817b9
Koodal Kweon <kkweon@f1r5s4.codam.nl>
2025-10-10 17:50:50 +0200

    ft_strrchr.c

--- ft_strrchar.c
+++ ft_strrchar.c
@@ -1,30 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchar.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/10 13:53:47 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/10 17:23:38 by kkweon           ###   ########.fr       */
+/*   Created: 2025/10/10 17:12:29 by kkweon            #+#    #+#             */
+/*   Updated: 2025/10/10 17:50:24 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *str, int init)
+int	ft_strlen(char *str)
 {
-	int i;
-	char *tmp_str;
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strrchr(const char *str, int init)
+{
+	int		i;
+	int		len;
+	char	*tmp_str;
 
 	i = 0;
 	// const variables can't be motified or returned.
 	// therefore need to assign and type cast the constant pointer to char pointer.
 	// as below:
 	tmp_str = (char *)str;
-	while (str[i] != '\0')
+	len = ft_strlen(tmp_str);
+	while (len > 0)
 	{
-		if (str[i] == init)
-			return (&tmp_str[i]);
-		i++;
+		if (str[len] == init)
+			return (&tmp_str[len]);
+		len--;
 	}
 	return (NULL);
 }
