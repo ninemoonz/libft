a5d8a62fa3ea483b8c971798170ea9149d43a2fd
Koodal Kweon <kkweon@f1r2s6.codam.nl>
2025-10-10 13:47:16 +0200

    creating ft_toupper.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 15:05:17 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/09 17:23:05 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/10/10 13:30:26 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,43 @@ void	*ft_memmove(void *dst, void *src, unsigned int n)
 	char			*tmp_dst;
 	char			*tmp_src;
 	unsigned int	i;
+	unsigned int	d_len;
 
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	tmp_dst = dst;
 	tmp_src = src;
 	i = 0;
-	if (dst == NULL && src == NULL)
+	if (tmp_dst > tmp_src)
 	{
-		return (NULL);
-	}
-	if (tmp_dst == tmp_src)
-	{
-		while (n > 0)
+		while (n-- > 0)
 		{
-			*tmp_dst = *tmp_src;
-			tmp_dst++;
-			tmp_src++;
-			n--;
+			tmp_dst[n] = tmp_src[n];
 		}
 	}
-	// else if (tmp_dst != tmp_src)
-	// {
-	// 	while (i < n)
-	// 	{
-	// 		i++;
-	// 	}
-	// }
+	else
+	{
+		while (i < n)
+		{
+			tmp_dst[i] = tmp_src[i];
+			i++;
+		}
+	}
 	return (dst);
 }
 
 int	main(void)
 {
 	char str_d[] = "This is destination";
+	char str_d2[] = "This is destination";
 	char str_s[] = "This is source";
 
-	// printf("str_d before: %s\n", str_d);
-	// memmove(str_d + 5, str_d, sizeof(str_s));
-	// printf("size of str_s: %lu\n", sizeof(str_s));
-	// printf("str_d after: %s\n", str_d);
-	// printf("str_s after: %s\n", str_s);
-
-	ft_memmove(str_d, str_s, sizeof(str_s));
-	printf("str_d after: %s\n", str_d);
+	printf("ft_ memmove str_d before: %s\n", str_d);
+	ft_memmove(str_d + 5, str_d, 5);
+	printf("ft_ memmove str_d after: %s\n", str_d);
+	printf("\n");
+	printf("memmove str_d2 before: %s\n", str_d2);
+	memmove(str_d2 + 5, str_d2, 5);
+	printf("memmove str_d2 after: %s\n", str_d2);
 	return (0);
 }
