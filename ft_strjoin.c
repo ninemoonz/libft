e0bb3dd36a5f702f9a62252dd6026935985e449e
Koodal Kweon <kkweon@f0r2s15.codam.nl>
2025-10-16 16:53:30 +0200

    ft_strjoin.c complete

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 16:16:08 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/16 16:27:55 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/10/16 16:53:08 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,25 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
 	int		j;
-	int		s1_len;
-	int		s2_len;
+	int		tot_len;
 	char	*dest;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	dest = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	tot_len = ft_strlen(s1) + ft_strlen(s2);
+	dest = (char *)malloc((tot_len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
 	i = 0;
 	j = 0;
-	while (i < (s1_len + s2_len))
+	while (s1[i])
 	{
-		while (s1[i])
-		{
-			dest[i] = s1[i];
-			i++;
-		}
-		while (s2[j])
-		{
-			dest[i] = s2[j];
-			i++;
-			j++;
-		}
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		dest[i] = s2[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
@@ -57,8 +54,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 int	main(void)
 {
-	char *str1 = "this is ";
-	char *str2 = "string join";
+	char *str1 = "this is a long strong string ";
+	char *str2 = "can you join this two?";
 	char *des;
 
 	des = ft_strjoin(str1, str2);
