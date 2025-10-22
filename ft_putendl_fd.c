52c56a4650429e844e3376b610c683ccf54431af
Koodal Kweon <kkweon@f0r2s3.codam.nl>
2025-10-22 14:29:30 +0200

    ft_putendl_fd.c

---
+++ ft_putendl_fd.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 14:25:28 by kkweon            #+#    #+#             */
+/*   Updated: 2025/10/22 14:29:14 by kkweon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+
+int	main(void)
+{
+	char *str = "Hello World";
+	int fd = 1;
+
+	ft_putendl_fd(str, fd);
+	return (0);
+}
