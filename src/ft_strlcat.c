b7929a64582207c20cc2bd0173ead3985ac3328a
Koodal Kweon <kkweon@f0r3s9.codam.nl>
2025-10-27 14:44:54 +0100

    ft_strlcat fix

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 14:38:03 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/24 18:19:03 by koodal           ###   ########.fr       */
+/*   Updated: 2025/10/27 14:44:24 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t dst_len;
-	size_t src_len;
-	size_t i;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	if (dst_len >= dstsize)
-		return (dstsize + src_len);
+		return (dst_len + src_len);
 	i = 0;
-	while (dstsize > (dst_len + 1 + i) && src[i])
+	while ((dstsize > (dst_len + 1 + i)) && src[i])
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
-	dst[dst_len + i] = 0;
+	dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
 }
--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -3,14 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 12:56:08 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/23 15:22:22 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/10/27 14:43:50 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+//#include <libft.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char	*ft_strnstr(const char *big, const char *small, size_t len)
 {
@@ -27,19 +30,27 @@ char	*ft_strnstr(const char *big, const char *small, size_t len)
 	while (tmp_big[i] && i < len)
 	{
 		j = 0;
-		while (tmp_big[i + j] == tmp_small[j])
+		while ((tmp_big[i + j] == tmp_small[j]) && i + j < len)
 		{
-			if (i + j < len)
+			if (tmp_big[i + j] != '\0')
 			{
-				if (tmp_big[i + j] != '\0')
-				{
-					if (tmp_small[j + 1] == '\0')
-						return (&tmp_big[i]);
-				}
+				if (tmp_small[j + 1] == '\0')
+					return (&tmp_big[i]);
 			}
 			j++;
 		}
 		i++;
 	}
 	return (NULL);
+}
+
+int	main(void)
+{
+	char	*haystack;
+	char	*needle;
+
+	haystack = "This is a haystack. try find needle in here";
+	needle = "needle";
+	printf("%s\n", ft_strnstr(haystack, needle, 5));
+	return (0);
 }
