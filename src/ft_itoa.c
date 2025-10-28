9d12a754420a56a7d08ea17a983668f810c7f32a
Koodal Kweon <kkweon@f0r6s4.codam.nl>
2025-10-28 18:44:26 +0100

    creating ft_itoa.c file with prototype

---
+++ src/ft_itoa.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 18:33:32 by kkweon            #+#    #+#             */
+/*   Updated: 2025/10/28 18:36:26 by kkweon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int digit_gen(int n)
+{
+	
+}
+
+// integer to ascii value "char"
+char	*ft_itoa(int n)
+{
+	
+	return ();
+}
+
+int	main(void)
+{
+	int num;
+	char *charnum;
+
+	num = 123;
+	charnum = ft_itoa(num);
+	printf("%s\n", charnum);
+
+	return (0);
+}
