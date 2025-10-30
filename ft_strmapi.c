34c159d3a4df0dd1b5ee916125e1d720fd2f4a5b
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-10-30 15:38:18 +0100

    ft_strmapi.c edit for eval format

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,29 +6,11 @@
 /*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 14:52:14 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/30 14:33:08 by koodal           ###   ########.fr       */
+/*   Updated: 2025/10/30 15:37:52 by koodal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char test_func(unsigned int i, char c)
-{
-	if (c >= 'a' && c <= 'z' && i % 2 == 0)
-		return (c - 32);
-	else
-		return (c);
-}
-
-size_t ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
@@ -50,12 +32,4 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	}
 	tmp[i] = '\0';
 	return (tmp);
-}
-
-int	main(void)
-{
-	char	*str;
-	str = "hello world!";
-	printf("%s\n", ft_strmapi(str, test_func));
-	return (0);
 }
