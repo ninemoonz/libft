01d7433fc25aa0e4d002a1cad90fad353654c198
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2025-10-30 15:37:33 +0100

    ft_striteri.c for eval format

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,30 +6,11 @@
 /*   By: koodal <koodal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 14:34:28 by koodal            #+#    #+#             */
-/*   Updated: 2025/10/30 15:36:11 by koodal           ###   ########.fr       */
+/*   Updated: 2025/10/30 15:37:14 by koodal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-size_t ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-void test_func(unsigned int i, char *c)
-{
-	if (*c >= 'a' && *c <= 'z' && i % 2 == 0)
-		*c = *c - 32;
-}
+#include "libft.h"
 
 void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
@@ -41,12 +22,4 @@ void ft_striteri(char *s, void (*f)(unsigned int, char*))
 		(*f)(i, &s[i]);
 		i++;
 	}
-}
-
-int main (void)
-{
-	char str[] = "Hello, World!";
-	ft_striteri(str, test_func);
-	printf("%s\n", str);
-	return (0);
 }
