ead0f8024386ce6f6ff143e4c317dcf0f11f03fd
Koodal Kweon <kkweon@f0r1s9.codam.nl>
2025-11-17 16:19:46 +0100

    fixing ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:51:50 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/29 17:21:55 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/11/17 16:19:25 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	char	*s1_c;
-	char	*s2_c;
+	size_t				i;
+	const unsigned char	*s1_c;
+	const unsigned char	*s2_c;
 
 	i = 0;
-	s1_c = (char *)s1;
-	s2_c = (char *)s2;
-	while (i < n && s1[i] == s2[i])
+	s1_c = (const unsigned char *)s1;
+	s2_c = (const unsigned char *)s2;
+	while ((s1[i] || s2[i]) && (i < n))
+	{
+		if (s1_c[i] != s2_c[i])
+			return (s1_c[i] - s2_c[i]);
 		i++;
-	return (s1_c[i] - s2_c[i]);
+	}
+	return (0);
 }
