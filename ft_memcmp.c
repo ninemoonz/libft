b7fc111bcb6b2757c4822e4de3d7a9673efbb2f4
Koodal Kweon <kkweon@f0r1s9.codam.nl>
2025-11-17 16:48:13 +0100

    fixing ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 11:35:11 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/29 17:21:04 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/11/17 16:47:38 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,18 @@
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	char	*str1_tmp;
-	char	*str2_tmp;
-	size_t	i;
+	unsigned char	*str1_tmp;
+	unsigned char	*str2_tmp;
+	size_t			i;
 
-	str1_tmp = (char *)str1;
-	str2_tmp = (char *)str2;
+	str1_tmp = (unsigned char *)str1;
+	str2_tmp = (unsigned char *)str2;
 	i = 0;
-	if (str1_tmp != NULL && str2_tmp != NULL)
+	while (i < n)
 	{
-		while (i < n)
-		{
-			if (str1_tmp[i] == str2_tmp[i])
-				i++;
-			else
-				return (str1_tmp[i] - str2_tmp[i]);
-		}
+		if (str1_tmp[i] != str2_tmp[i])
+			return (str1_tmp[i] - str2_tmp[i]);
+		i++;
 	}
 	return (0);
 }
