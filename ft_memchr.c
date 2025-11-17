8fafe5b0f1c9ec9b62984412cd6d861c53f2cf37
Koodal Kweon <kkweon@f0r1s9.codam.nl>
2025-11-17 16:39:52 +0100

    fixing ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 11:14:17 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/29 17:21:03 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/11/17 16:38:58 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 
 void	*ft_memchr(const void *str, int c, size_t n)
 {
-	char	*tmp_str;
-	size_t	i;
+	unsigned char	*tmp_str;
+	unsigned char	needle;
+	size_t			i;
 
-	tmp_str = (char *)str;
+	tmp_str = (unsigned char *)str;
+	needle = (unsigned char)c;
 	i = 0;
 	if (n == 0)
 		return (NULL);
-	while (i <= n && tmp_str[i] != c)
+	while (i < n)
 	{
+		if (tmp_str[i] == needle)
+			return ((void *)&tmp_str[i]);
 		i++;
 	}
-	return (&tmp_str[i]);
+	return (NULL);
 }
