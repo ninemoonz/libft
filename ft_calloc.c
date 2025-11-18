d69bbe0d979bf283b1a367d2f36608b458dc8421
Koodal Kweon <kkweon@f0r2s13.codam.nl>
2025-11-18 14:22:47 +0100

    fixing ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 11:27:29 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/29 17:20:38 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/11/18 12:46:02 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	*ft_calloc(size_t nitems, size_t size)
 
 	i = 0;
 	tmp = malloc(nitems * size);
-	if (tmp == NULL)
+	if (!tmp)
 		return (NULL);
-	while (i < nitems)
+	while (i < nitems * size)
 	{
 		tmp[i] = 0;
 		i++;
