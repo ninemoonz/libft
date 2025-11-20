ccdffa4475b3d07d64f1a7031bcbaa76fc505222
Koodal Kweon <kkweon@f1r3s7.codam.nl>
2025-11-20 16:43:30 +0100

    making overlap check function

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,25 @@
 /*   By: kkweon <kkweon@student.codam.nl>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 15:05:17 by kkweon            #+#    #+#             */
-/*   Updated: 2025/10/29 17:21:08 by kkweon           ###   ########.fr       */
+/*   Updated: 2025/11/20 16:36:33 by kkweon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	overlap_check(char *dst, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (&src[i] == dst)
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
@@ -21,9 +35,9 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	if (!dst && !src)
 		return (NULL);
 	tmp_dst = (char *)dst;
-	tmp_src = (char *)src;
+	tmp_src = (const char *)src;
 	i = 0;
-	if (tmp_dst > tmp_src)
+	if (overlap_check(tmp_dst, tmp_src, n) > 0)
 	{
 		while (n-- > 0)
 			tmp_dst[n] = tmp_src[n];
